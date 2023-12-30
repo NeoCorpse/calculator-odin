e2825fbe7b41d7413c4f230b26ccf6b98fd548da
Ian Wafula <neocorpse@proton.me>
2023-12-30 12:53:09 +0300

    Fix bugs

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ const former = document.querySelector('.span1')
 const equals = document.querySelector('.equals')
 const period = document.querySelector('.dot')
 const myOps = ['+', '-', '*', '/'] 
+const myOperators = {
+    sum(a, b) {
+        return +a + +b
+    },
+    difference(a, b) {
+        return a-b
+    },
+    multiply(a,b) {
+        return a * b
+    },
+    divide(a,b) {
+        return a / b
+    }
+}
 
 let num = 0;
 let resultDisplayed = false;
@@ -29,6 +43,10 @@ numBtns.forEach(button => {
         if (display.textContent == 0 && button.textContent == 0) return
         if (display.textContent == 0) display.textContent = ''
         if (display.textContent) clear
+        if (resultDisplayed === true) {
+            display.textContent = ''
+            former.textContent = ''
+        }
         let value = button.textContent
         display.textContent += value;
         resultDisplayed = false;
@@ -38,65 +56,34 @@ numBtns.forEach(button => {
 operators.forEach(button => {
     button.addEventListener('click', () => {
         if (display.textContent == 0) return
+        if (resultDisplayed === true) {
+            former.textContent = display.textContent
+            display.textContent = ''
+            console.log('woohoo')
+        }
         let value = button.textContent
         former.textContent += `${display.textContent} ${value} `;
         display.textContent = 0
         resultDisplayed = false
+
+        
     })
 })
 
-const myOperators = {
-    sum(a, b) {
-        return +a + +b
-    },
-    difference(a, b) {
-        return a-b
-    },
-    multiply(a,b) {
-        return a * b
-    },
-    divide(a,b) {
-        return a / b
-    }
-}
-
 equals.addEventListener('click', () => {
+    if (!former.textContent.includes('=')) {
+        calculate();
+    former.textContent += '=';
+    }
+})
+
+function calculate() {
     former.textContent += `${display.textContent}`
     let x = former.textContent.split(' ')
-    // if (myOps.includes(x[-1])) {
-        
-    // }
+
     let operators = x.filter(val => myOps.includes(val))
     let numbers = x.filter(val => !myOps.includes(val))
-    let result = 0, current;
-    let prev;
     let i = 0;
-
-    // for (let i = 0; i < numbers.length; i++) {
-    //     current = numbers[i]
-    //     if (!prev) {
-    //         prev = current
-    //         continue;
-    //     }
-    //     switch (operators[i-1]) {
-    //         case '+':
-    //             result = myOperators.sum(current, result)
-    //             prev = current
-    //             break;
-    //         case '-':
-    //             result = myOperators.difference(current, prev)
-    //             prev = current
-    //             break;
-    //         case '/':
-    //             result = myOperators.divide(current, prev)
-    //             prev = current
-    //             break;
-    //         case '*':
-    //             result = myOperators.multiply(current, prev)
-    //         default:
-    //             break;
-    //     }
-    // }
 
     let myResult = numbers.reduce((total, number) => {
         switch(operators[i]) {
@@ -118,7 +105,7 @@ equals.addEventListener('click', () => {
 
     },)
 
-    display.textContent = myResult
+    display.textContent = `${myResult}`
     console.log(myResult)
-    resultDisplayed = true
-})
+    resultDisplayed = true 
+}
